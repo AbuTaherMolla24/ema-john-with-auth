85f819edff83c4fc5eb2f9062c0b57f1fba0fd81
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-10-07 17:40:29 +0600

    com

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVD3r7kIlZX9PIFXfrANMbMfDFQ2k4tSM",
+  authDomain: "ema-john-with-auth-ad060.firebaseapp.com",
+  databaseURL: "https://ema-john-with-auth-ad060.firebaseio.com",
+  projectId: "ema-john-with-auth-ad060",
+  storageBucket: "ema-john-with-auth-ad060.appspot.com",
+  messagingSenderId: "333384640231",
+  appId: "1:333384640231:web:a51c8722020f50bea76442"
+};
+export default firebaseConfig;
